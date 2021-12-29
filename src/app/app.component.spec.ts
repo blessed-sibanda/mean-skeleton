69dc84e4d5df3d3393a5c180c1bb61c1c8325e7f
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-29 15:45:09 +0200

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'material-app'`, () => {
+  it(`should have as title 'skeleton'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('material-app');
+    expect(app.title).toEqual('skeleton');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('material-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'skeleton app is running!'
+    );
   });
 });
